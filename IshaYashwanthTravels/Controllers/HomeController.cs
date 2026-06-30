5f7bb702dd49bc72e32ca615a39cbc401d96a692
Mohan <mohan0348926@gmail.com>
2026-06-30 23:18:20 +0530

    static website

--- IshaYashwanthTravels/Controllers/HomeController.cs
+++ IshaYashwanthTravels/Controllers/HomeController.cs
@@ -1,45 +1,12 @@
-using IshaYaswanthTravels.Models;
-using IshaYaswanthTravels.Services;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
-namespace IshaYaswanthTravels.Controllers
+namespace IshaYashwanthTravels.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly EmailService _emailService;
-
-        public HomeController(EmailService emailService)
-        {
-            _emailService = emailService;
-        }
-
         public IActionResult Index()
         {
             return View();
-        }
-
-
-        [HttpPost]
-        public async Task<IActionResult> Contact(ContactViewModel model)
-        {
-            try
-            {
-                await _emailService.SendContactMail(model);
-
-                return Json(new
-                {
-                    success = true,
-                    message = "Enquiry sent successfully!"
-                });
-            }
-            catch (Exception ex)
-            {
-                return Json(new
-                {
-                    success = false,
-                    message = ex.Message
-                });
-            }
         }
     }
 }
--- IshaYashwanthTravels/Program.cs
+++ IshaYashwanthTravels/Program.cs
@@ -1,14 +1,13 @@
-using IshaYaswanthTravels.Services;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
-builder.Services.AddScoped<EmailService>();
+
 
 var app = builder.Build();
 
-var port = Environment.GetEnvironmentVariable("PORT") ?? "10000";
-app.Urls.Add($"http://*:{port}");
+
 
 app.UseStaticFiles();
 app.UseRouting();
